638329f7f04ba0e278da7538c997c6c777e054d9
Minyoungkyu <alsdudrb123@gmail.com>
2023-08-15 18:16:59 +0900

    setting querydsl , adding member entity

--- db/schema.sql
+++ db/schema.sql
@@ -8,4 +8,18 @@ CREATE TABLE article (
 	title CHAR(100) NOT NULL,
 	`body` TEXT NOT NULL);
 
+CREATE TABLE `member` (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    loginId CHAR(20) NOT NULL,
+	loginPw CHAR(60) NOT NULL,
+	`authLevel` SMALLINT(2) UNSIGNED DEFAULT 3 COMMENT '(3=일반, 7=관리자)',
+	`name` CHAR(20) NOT NULL,
+	nickname CHAR(20) NOT NULL,
+	cellphoneNo CHAR(20) NOT NULL,
+	email CHAR(50) NOT NULL,
+	delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '탈퇴여부(0=탈퇴전, 1=탈퇴)',
+	delDate DATETIME COMMENT '탈퇴날짜'
+);
 
